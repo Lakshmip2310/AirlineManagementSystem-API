e96fe2aefccb0e7873a1a2341d0639e353856a92
Sahil3343 <sahil3343@gmail.com>
2021-10-06 12:27:26 +0530

    Initial Commit

---
+++ src/main/java/com/sahilmahajan/airlinemsapi/AirlineMsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sahilmahajan.airlinemsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirlineMsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirlineMsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sahilmahajan/airlinemsapi/AirlineMsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahilmahajan.airlinemsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirlineMsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
